02f6e374bd6d874d23122674ee26631138d5eb28
Andrew Klofas <aklofas@gmail.com>
2018-11-20 22:18:47 -0800

    Started motor json api

---
+++ firmware/wifistepper/wifistepper.h
@@ -0,0 +1,18 @@
+#ifndef __WIFISTEPPER_H
+#define __WIFISTEPPER_H
+
+#include "powerstep01.h"
+
+typedef struct __cfg_motor {
+  ps_mode mode;
+  ps_stepsize stepsize;
+  float maxspeed, minspeed;
+  float accel, decel;
+  float kthold, ktrun, ktaccel, ktdecel;
+  
+  float fullstepspeed;
+  bool fsboost;
+} cfg_motor;
+
+#endif
+
